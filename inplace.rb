1e6f054fc680b37ae77f565490ba81c5666eb455
Akinori MUSHA <knu@idaemons.org>
2008-09-17 08:18:21 +0000

    reduce an if

--- inplace.rb
+++ inplace.rb
@@ -361,19 +361,19 @@ class FileFilter
   def replace(file1, file2, stat)
     file2_is_original = !tmpfile?(file2)
 
-    if $backup_suffix && !$backup_suffix.empty? && file2_is_original
-      bakfile = file2 + $backup_suffix
-
-      if $preserve_inode
-        debug "copy: %s -> %s", file2.shellescape, bakfile.shellescape
-        FileUtils.cp(file2, bakfile) unless $dry_run 
-      else
-        debug "move: %s -> %s", file2.shellescape, bakfile.shellescape
-        FileUtils.mv(file2, bakfile) unless $dry_run
-      end
-    end
-
     if file2_is_original
+      if $backup_suffix && !$backup_suffix.empty?
+        bakfile = file2 + $backup_suffix
+
+        if $preserve_inode
+          debug "copy: %s -> %s", file2.shellescape, bakfile.shellescape
+          FileUtils.cp(file2, bakfile) unless $dry_run 
+        else
+          debug "move: %s -> %s", file2.shellescape, bakfile.shellescape
+          FileUtils.mv(file2, bakfile) unless $dry_run
+        end
+      end
+
       begin
         if $preserve_inode
           debug "copy: %s -> %s", file1.shellescape, file2.shellescape
