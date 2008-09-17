a1860c342d5998b816a426876ad39048e9874c3a
Akinori MUSHA <knu@idaemons.org>
2008-09-17 13:56:30 +0000

    - FileUtils.mv does not have a :preserve option. - FileUtils.cp does not need  :preserve => false because that's the default.

--- inplace.rb
+++ inplace.rb
@@ -355,7 +355,7 @@ class FileFilter
   def replace(file1, file2, stat)
     if tmpfile?(file2)
       debug "move: %s -> %s", file1.shellescape, file2.shellescape
-      FileUtils.mv(file1, file2, :preserve => false)
+      FileUtils.mv(file1, file2)
     else
       if $backup_suffix && !$backup_suffix.empty?
         bakfile = file2 + $backup_suffix
@@ -372,12 +372,12 @@ class FileFilter
       begin
         if $preserve_inode
           debug "copy: %s -> %s", file1.shellescape, file2.shellescape
-          FileUtils.cp(file1, file2, :preserve => false) unless $dry_run
+          FileUtils.cp(file1, file2) unless $dry_run
           debug "remove: %s", file1.shellescape
           FileUtils.rm(file1) unless $dry_run
         else
           debug "move: %s -> %s", file1.shellescape, file2.shellescape
-          FileUtils.mv(file1, file2, :preserve => false) unless $dry_run
+          FileUtils.mv(file1, file2) unless $dry_run
         end
       rescue => e
         error "%s: failed to overwrite: %s", file2, e
